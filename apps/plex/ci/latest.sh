86cc45164a6b1e8edea797803b39ed5117aa60d3
Kilian Tyler <github@ktyler.me>
2024-03-02 21:55:30 -0500

    fix: mac friendly in this house

--- apps/plex/ci/latest.sh
+++ apps/plex/ci/latest.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 channel=$1
 
+# if running on a linux, use grep
+GREP_CMD="grep"
+
+# if running on a mac, use ggrep
+OS_TYPE=$(uname)
+if [[ "${OS_TYPE}" == "Darwin" ]]; then
+    GREP_CMD="ggrep"
+fi
+
 if [[ "${channel}" == "beta" ]]; then
     git clone --quiet --depth=1 https://aur.archlinux.org/plex-media-server-plexpass.git /tmp/plex-media-server-plexpass
     pushd /tmp/plex-media-server-plexpass > /dev/null || exit
-    version="$(grep -oP "(?<=pkgver=).*" PKGBUILD)"
-    version="${version}-$(grep -oP "(?<=_pkgsum=).*" PKGBUILD)"
+    version="$(${GREP_CMD} -oP "(?<=pkgver=).*" PKGBUILD)"
+    version="${version}-$(${GREP_CMD} -oP "(?<=_pkgsum=).*" PKGBUILD)"
     popd > /dev/null || exit
     rm -rf /tmp/plex-media-server-plexpass
 fi
--- apps/postgres-init/ci/latest.sh
+++ apps/postgres-init/ci/latest.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql16-client&branch=v3.19&arch" | grep -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
+# if running on a linux, use grep
+GREP_CMD="grep"
+
+# if running on a mac, use ggrep
+OS_TYPE=$(uname)
+if [[ "${OS_TYPE}" == "Darwin" ]]; then
+    GREP_CMD="ggrep"
+fi
+
+version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql16-client&branch=v3.19&arch" | ${GREP_CMD} -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
