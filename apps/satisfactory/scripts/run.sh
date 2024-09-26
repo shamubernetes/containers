697b1b999186a2940d1ae4e5f8f4f148c3bab829
Kilian Tyler <kilian@kil.dev>
2024-09-26 13:04:09 -0400

    fix: satisfactory

--- apps/satisfactory/scripts/run.sh
+++ apps/satisfactory/scripts/run.sh
@@ -2,35 +2,22 @@
 
 source /app/scripts/lib.sh
 
-AUTOSAVENUM="5"
-MAXOBJECTS="2162688"
-MAXTICKRATE="120"
-TIMEOUT="300"
-TIMEOUT="300"
-SERVERSTREAMING="0"
-MAXPLAYERS="4"
-DISABLESEASONALEVENTS=""
-SKIPUPDATE="false"
-STEAMBETAFLAG="public"
-
-# ini_args=(
-#   "-ini:Engine:[Core.Log]:LogNet=Error"
-#   "-ini:Engine:[Core.Log]:LogNetTraffic=Warning"
-#   "-ini:Engine:[/Script/FactoryGame.FGSaveSession]:mNumRotatingAutosaves=$AUTOSAVENUM"
-#   "-ini:Engine:[/Script/Engine.GarbageCollectionSettings]:gc.MaxObjectsInEditor=$MAXOBJECTS"
-#   "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:LanServerMaxTickRate=$MAXTICKRATE"
-#   "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:NetServerMaxTickRate=$MAXTICKRATE"
-#   "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:ConnectionTimeout=$TIMEOUT"
-#   "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:InitialConnectTimeout=$TIMEOUT"
-#   "-ini:Engine:[ConsoleVariables]:wp.Runtime.EnableServerStreaming=$SERVERSTREAMING"
-#   "-ini:Game:[/Script/Engine.GameSession]:ConnectionTimeout=$TIMEOUT"
-#   "-ini:Game:[/Script/Engine.GameSession]:InitialConnectTimeout=$TIMEOUT"
-#   "-ini:Game:[/Script/Engine.GameSession]:MaxPlayers=$MAXPLAYERS"
-#   "-ini:GameUserSettings:[/Script/Engine.GameSession]:MaxPlayers=$MAXPLAYERS"
-#   "$DISABLESEASONALEVENTS"
-# )
-
-/app/steamcmd/steamcmd.sh +force_install_dir /config/gamefiles +login anonymous +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate +quit
+ini_args=(
+  "-ini:Engine:[Core.Log]:LogNet=Error"
+  "-ini:Engine:[Core.Log]:LogNetTraffic=Warning"
+  "-ini:Engine:[/Script/FactoryGame.FGSaveSession]:mNumRotatingAutosaves=$AUTOSAVENUM"
+  "-ini:Engine:[/Script/Engine.GarbageCollectionSettings]:gc.MaxObjectsInEditor=$MAXOBJECTS"
+  "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:LanServerMaxTickRate=$MAXTICKRATE"
+  "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:NetServerMaxTickRate=$MAXTICKRATE"
+  "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:ConnectionTimeout=$CONNECTION_TIMEOUT"
+  "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:InitialConnectTimeout=$INITIAL_CONNECT_TIMEOUT"
+  "-ini:Engine:[ConsoleVariables]:wp.Runtime.EnableServerStreaming=$SERVERSTREAMING"
+  "-ini:Game:[/Script/Engine.GameSession]:ConnectionTimeout=$CONNECTION_TIMEOUT"
+  "-ini:Game:[/Script/Engine.GameSession]:InitialConnectTimeout=$INITIAL_CONNECT_TIMEOUT"
+  "-ini:Game:[/Script/Engine.GameSession]:MaxPlayers=$MAXPLAYERS"
+  "-ini:GameUserSettings:[/Script/Engine.GameSession]:MaxPlayers=$MAXPLAYERS"
+  "$DISABLESEASONALEVENTS"
+)
 
 printf "Launching game server\\n\\n"
 
